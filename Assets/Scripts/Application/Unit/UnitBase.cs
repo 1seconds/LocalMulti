21a15eaf38cb795073a76b4e325e19607b982daf
usung <1seconds@naver.com>
2022-01-01 17:12:21 +0900

    implement stage service, unit service

---
+++ Assets/Scripts/Application/Unit/UnitBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitBase : MonoBehaviour {
+    public Dictionary<int, Unit> myUnits;
+    private SceneType currentScene;
+
+    private void OnEnable() {
+        Service.unit.selectedUnitUpdate += OnUpdateSelectedUnit;
+    }
+
+    private void OnDisable() {
+        Service.unit.selectedUnitUpdate -= OnUpdateSelectedUnit;
+    }
+
+    private void OnUpdateSelectedUnit(JobType type) {
+    }
+    
+    private void ReadyData() {
+    }
+    private void Start() {
+        ReadyData();
+    }
+}
--- Assets/Scripts/Application/Unit/UnitController.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-public class UnitController : MonoBehaviour {
-    public Dictionary<int, Unit> myUnits;
-
-    public void Awake() {
-    }
-}
